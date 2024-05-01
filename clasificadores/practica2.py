cc13bf6482a543a01680875c7f003b3076078a1e
ReneGarBer <67211759+ReneGarBer@users.noreply.github.com>
2024-05-01 14:07:29 -0600

    Update practica2.py

--- clasificadores/practica2.py
+++ clasificadores/practica2.py
@@ -1,82 +1,187 @@
-import metricas as mt
 import pandas as pd
+import warnings
 
 from sklearn.neighbors import KNeighborsClassifier, kneighbors_graph
 from sklearn.linear_model import LogisticRegression 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split 
-from sklearn.svm import OneClassSVM 
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def categorizar_y(y):
+    if y <= quartiles.iloc[0]:
+        return 'c'
+    elif y > quartiles.iloc[0] and y <= quartiles.iloc[1]:
+        return 'b'
+    else:
+        return 'a'
+
+def print_results(clasificador,metrics):
+    print("\nClasificador: ",clasificador)
+    print("Accuracy: ", metrics[0],"%")
+    print("Precision:", metrics[1],"%")
+    print("Recall:", metrics[2],"%")
+    print("F1 Score:", metrics[3],"%")
+
+def get_metrics(y_test,y_predict):
+    metrics = []
+    metrics.append("{:.2f}".format(accuracy_score(y_test, y_predict)*100))
+    metrics.append("{:.2f}".format(precision_score(y_test, y_predict, average='weighted', zero_division='warn')*100))
+    metrics.append("{:.2f}".format(recall_score(y_test, y_predict, average='weighted')*100))
+    metrics.append("{:.2f}".format(f1_score(y_test, y_predict, average='weighted')*100))
+    return metrics
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 #leer data
 excel_file = 'Datasets.xlsx'
 sheet1,sheet2,sheet3 = 'AutoInsuranceSweden','WineQualityDataset','PimaIndiansDiabetesDataset'
 
 data_1 = pd.read_excel(excel_file,sheet_name=sheet1)
-#print(data)
+quartiles = data_1['Y'].quantile([0.25,0.5,0.75])
+
+data_1['categoria_y'] = data_1['Y'].apply(categorizar_y)
 
 data_2 = pd.read_excel(excel_file,sheet_name=sheet2)
-#print(data_2)
 
 data_3 = pd.read_excel(excel_file,sheet_name=sheet3)
-#print(data)
 
-#Dividir los datos en features y target
-#Auto Insurance in Sweden ais
-features_ais = data_1.drop('Y',axis=1)
-target_ais = data_1['Y']
-# print(features_ais)
-# print(target_ais)
+#Auto Insurance in Sweden
+print('Auto Insurance in Sweden')
+features_ais = data_1[['X']]
+target_ais = data_1['categoria_y']
+x_train, x_test, y_train, y_test = train_test_split(features_ais, target_ais, test_size=0.2, random_state=42)
 
-#Wine Quaality
-features_wine = data_2.drop(columns='quality')
-target_wine = data_2['quality']
-# print(features_wine)
-# print(target_wine)
+#Regresión logística
+log_regression = LogisticRegression(max_iter=500)#Iniciar
+log_regression.fit(x_train,y_train)#Entrenar
+y_predict = log_regression.predict(x_test)#Predecir
 
-#Pima Indians Diabetes Database
-features_diabetes = data_3.drop(columns='Class variable (0 or 1)')
-target_diabetes = data_3['Class variable (0 or 1)']
-# print(features_wine)
-# print(target_diabetes)
+metrics = get_metrics(y_test,y_predict)
+print_results('Regresión logística',metrics)
 
-#Dividir los datos en training y testing
-x_train, x_test, y_train, y_test = train_test_split(features_diabetes, target_diabetes, test_size=0.2, random_state=42)
+#K-Neighbors
+k_neighbors = KNeighborsClassifier()#Iniciar
+k_neighbors.fit(x_train,y_train)#Entrenar
+y_pred1 = k_neighbors.predict(x_test)#Predecir
 
+metrics = get_metrics(y_test,y_predict)
+print_results('K-Vecinos Cercanos',metrics)
 
-#analizar datos con los métodos
-#Regresión logística (Logistic Regression)
-#Óptima para el dataset Pima Indians Diabetes Database dado que los datos de entrada son continuos y el de salida es binario
-#Iniciar
-log_regression = LogisticRegression(max_iter=200)
+#Maquinas Vector Soporte
+svc = SVC()#Iniciar
+svc.fit(x_train,y_train)#Entrenar
+y_pred1 = svc.predict(x_test)#Predecir
 
-#Entrenar
-log_regression.fit(features_diabetes,target_diabetes)
-
-#Predecir
-y_pred = log_regression.predict(x_test)
-print(y_pred)
-#K-Vecinos Cercanos (K-Nearest Neighbors)
-
-#Maquinas Vector Soporte (Support Vector Machines)
+metrics = get_metrics(y_test,y_predict)
+print_results('Maquinas Vector Soporte',metrics)
 
 #Naive Bayes
-#Óptimo para el dataset Auto Insourance Sweden
+naive_bayes = GaussianNB()#Iniciar
+naive_bayes.fit(x_train,y_train)#Entrenar
+y_pred1 = naive_bayes.predict(x_test)#Predecir
 
-#Iniciar
-naive_bayes = GaussianNB()
+metrics = get_metrics(y_test,y_predict)
+print_results('Naive Bayes',metrics)
 
-#Entrenar
-#naive_bayes.fit(x_train,y_train)
+#MLPClassifier
+mlp_classifier = MLPClassifier()#Iniciar
+mlp_classifier.fit(x_train,y_train)#Entrenar
+y_pred1 = mlp_classifier.predict(x_test)#Predecir
 
-#Predecir
-#y_pred = naive_bayes.predict(x_test)
+metrics = get_metrics(y_test,y_predict)
+print_results('MLPClassifier',metrics)
 
-#Red neuronal
+#Wine Quality
+print('\nWine Quality')
+features_wine = data_2.drop(columns='quality')
+target_wine = data_2['quality']
+x_train, x_test, y_train, y_test = train_test_split(features_wine, target_wine, test_size=0.2, random_state=42)
 
-#imprimir resultados
-#accuracy = accuracy_score(y_test, y_pred)
-#print("Accuracy:", accuracy)
+#Regresión logística
+log_regression = LogisticRegression(max_iter=200)#Iniciar
+log_regression.fit(x_train,y_train)#Entrenar
+y_predict = log_regression.predict(x_test)#Predecir
 
-# Mostrar el reporte de clasificación
-#print(classification_report(y_test, y_pred))
+metrics = get_metrics(y_test,y_predict)
+print_results('Regresión logística',metrics)
+
+#K-Neighbors
+k_neighbors = KNeighborsClassifier()#Iniciar
+k_neighbors.fit(x_train,y_train)#Entrenar
+y_pred1 = k_neighbors.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('K-Vecinos Cercanos',metrics)
+
+#Maquinas Vector Soporte
+svc = SVC()#Iniciar
+svc.fit(x_train,y_train)#Entrenar
+y_pred1 = svc.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('Maquinas Vector Soporte',metrics)
+
+#Naive Bayes
+naive_bayes = GaussianNB()#Iniciar
+naive_bayes.fit(x_train,y_train)#Entrenar
+y_pred1 = naive_bayes.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('Naive Bayes',metrics)
+
+#MLPClassifier
+mlp_classifier = MLPClassifier()#Iniciar
+mlp_classifier.fit(x_train,y_train)#Entrenar
+y_pred1 = mlp_classifier.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('MLPClassifier',metrics)
+
+#Pima Indians Diabetes Database
+print('\nPima Indians Diabetes Database')
+features_db = data_3.drop(columns='Class variable (0 or 1)')
+target_db = data_3['Class variable (0 or 1)']
+x_train, x_test, y_train, y_test = train_test_split(features_db, target_db, test_size=0.2, random_state=42)
+
+#Regresión logística
+log_regression = LogisticRegression(max_iter=200)#Iniciar
+log_regression.fit(x_train,y_train)#Entrenar
+y_predict = log_regression.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('Regresión logística',metrics)
+
+#K-Neighbors
+k_neighbors = KNeighborsClassifier()#Iniciar
+k_neighbors.fit(x_train,y_train)#Entrenar
+y_pred1 = k_neighbors.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('K-Vecinos Cercanos',metrics)
+
+#Maquinas Vector Soporte
+svc = SVC()#Iniciar
+svc.fit(x_train,y_train)#Entrenar
+y_pred1 = svc.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('Maquinas Vector Soporte',metrics)
+
+#Naive Bayes
+naive_bayes = GaussianNB()#Iniciar
+naive_bayes.fit(x_train,y_train)#Entrenar
+y_pred1 = naive_bayes.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('Naive Bayes',metrics)
+
+#MLPClassifier
+mlp_classifier = MLPClassifier()#Iniciar
+mlp_classifier.fit(x_train,y_train)#Entrenar
+y_pred1 = mlp_classifier.predict(x_test)#Predecir
+
+metrics = get_metrics(y_test,y_predict)
+print_results('MLPClassifier',metrics)
